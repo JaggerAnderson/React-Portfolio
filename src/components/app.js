cb6f07194243642ca703470bbb84a7bdb29f2dc2
codestudent <codestudent@708S-G104-116.canyonsdistrict.pvt>
2019-07-03 09:11:05 -0600

    Initial Commit

---
+++ src/components/app.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import moment from "moment";
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+      <h1>Jagger Anderson Portfolio</h1>
+        <div>
+          {moment().format('MMM Do YYYY, h:mm:ss a')}
+        </div>
+      </div>
+    );
+  }
+}
